9f7ffa558bb2c3d7debdee4658f214fcb8c75998
Kamlesh <kamlesh.send@gmail.com>
2014-11-09 02:05:23 +0530

    First commit

---
+++ demo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+   <?php 
+	 echo $_SERVER['REMOTE_ADDR'];
+	 while (list ($key, $val) = each ($_SERVER)) {
+		$key -> $val
+	}
+ ?> 
+</body>
+</html>
